d755be41077b7ca8bd079464f008a9871d05f5e5
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-05-27 10:16:00 +0200

    Adding configuration for "A Bunny and Carrots".

---
+++ config/easy/ABunnyAndCarrots/code/CGCode.js
@@ -0,0 +1,15 @@
+var inputs = readline().split(' ');
+const M = parseInt(inputs[0]);
+const N = parseInt(inputs[1]);
+const t = parseInt(readline());
+var inputs = readline().split(' ');
+for (let i = 0; i < t; i++) {
+    const choices = parseInt(inputs[i]);
+}
+for (let i = 0; i < t; i++) {
+
+    // Write an answer using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log('perimeter');
+}
---
+++ config/easy/ABunnyAndCarrots/code/CGCode.php
@@ -0,0 +1,16 @@
+<?php
+fscanf($stdin, "%d %d", $M, $N);
+fscanf($stdin, "%d", $t);
+$inputs = explode(" ", fgets($stdin));
+for ($i = 0; $i < $t; $i++)
+{
+    $choices = intval($inputs[$i]);
+}
+for ($i = 0; $i < $t; $i++)
+{
+
+    // Write an answer using echo(). DON'T FORGET THE TRAILING \n
+    // To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+    echo("perimeter\n");
+}
