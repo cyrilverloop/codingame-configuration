f54afd7ae16f16ae785cdb666f78c7c1282cc873
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-06-03 10:21:22 +0200

    Adding configuration for "Six Degrees of Kevin Bacon".

---
+++ config/easy/SixDegreesOfKevinBacon/code/CGCode.js
@@ -0,0 +1,10 @@
+const actorName = readline();
+const n = parseInt(readline());
+for (let i = 0; i < n; i++) {
+    const movieCast = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('N degrees to Kevin Bacon');
