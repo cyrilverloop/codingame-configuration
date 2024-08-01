4331c41951ea391820eca3d0ae0c8dc5925be0be
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-08-01 10:40:13 +0200

    Adding configuration for "Interstellar".

---
+++ config/easy/Interstellar/code/CGCode.js
@@ -0,0 +1,12 @@
+const ship = readline();
+const wormhole = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('Direction:');
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('Distance:');
---
+++ config/easy/Interstellar/code/CGCode.php
@@ -0,0 +1,13 @@
+<?php
+$ship = stream_get_line(STDIN, 20 + 1, "\n");
+$wormhole = stream_get_line(STDIN, 20 + 1, "\n");
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("Direction:\n");
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("Distance:\n");
