aded8f65bb8cc2ad26ac0ab600dfdd32a0547ef8
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-09-05 08:20:50 +0200

    Adding configuration for "Rummikub 1".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - configuration for "ASCII Art with Logo Language".
 - configuration for "ASCII Art with Logo Language, Part 2".
+- configuration for "Rummikub 1".
 
 ## [1.6.0] - 2024-09-02
 ### Added
---
+++ config/medium/Rummikub1/code/CGCode.php
@@ -0,0 +1,13 @@
+<?php
+$goaltile = stream_get_line(STDIN, 3 + 1, "\n");
+fscanf(STDIN, "%d", $nrow);
+for ($i = 0; $i < $nrow; $i++)
+{
+    $row = stream_get_line(STDIN, 42 + 1, "\n");
+}
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("PUT tile rowid\n");
+echo("rowid tile tile tile\n");
