bdf47fc28b7ae25b0aea72ebb20fab5a24b68c3e
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-09-17 08:32:20 +0200

    Adding configuration for "Firewall placement".

---
+++ config/medium/FirewallPlacement/code/CGCode.js
@@ -0,0 +1,11 @@
+const numNodes = parseInt(readline());
+const virusLocation = parseInt(readline());
+const numLinks = parseInt(readline());
+for (let i = 0; i < numLinks; i++) {
+    const link = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
