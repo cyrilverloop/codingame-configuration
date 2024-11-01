d7edfb5840f90ebf3b3ae5265c27c20fd9341685
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-11-01 11:03:17 +0100

    Adding configuration for "Binary Tree Visual".

---
+++ config/medium/BinaryTreeVisual/code/CGCode.js
@@ -0,0 +1,12 @@
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    var inputs = readline().split(' ');
+    const V = parseInt(inputs[0]);
+    const IDLEFT = parseInt(inputs[1]);
+    const IDRIGHT = parseInt(inputs[2]);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
