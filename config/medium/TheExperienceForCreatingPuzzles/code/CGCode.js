eda77ca8c12bfd9445d268101ac2dd825533342f
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-11-11 10:20:49 +0100

    Adding configuration for "The experience for creating puzzles".

---
+++ config/medium/TheExperienceForCreatingPuzzles/code/CGCode.js
@@ -0,0 +1,8 @@
+const level = parseInt(readline());
+const xp = parseInt(readline());
+const N = parseInt(readline());
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
