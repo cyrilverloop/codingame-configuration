c2576f41de2b9c6b7d24dc533ff5d2ec3070f9c5
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-11-29 10:05:55 +0100

    Adding configuration for "Fractal Image Recognition".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -34,6 +34,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Straddling Checkerboard Cryptography".
 - configuration for "Halting sequences".
 - configuration for "Christmas Tree".
+- configuration for "Fractal Image Recognition".
 
 ## [1.8.0] - 2024-10-31
 ### Added
---
+++ config/medium/FractalImageRecognition/code/CGCode.js
@@ -0,0 +1,12 @@
+const imageSize = parseInt(readline());
+for (let i = 0; i < imageSize; i++) {
+    var inputs = readline().split(' ');
+    for (let j = 0; j < imageSize; j++) {
+        const pixel = parseInt(inputs[j]);
+    }
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
