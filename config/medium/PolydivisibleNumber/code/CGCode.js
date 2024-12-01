0febd9f9ec44ee53d9ea03de956bc8da401fbd78
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-12-01 11:24:35 +0100

    Adding configuration for "Polydivisible number".

---
+++ config/medium/PolydivisibleNumber/code/CGCode.js
@@ -0,0 +1,8 @@
+const NUMBER = readline();
+for (let i = 0; i < nbanswer; i++) {
+
+    // Write an answer using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log('value');
+}
---
+++ config/medium/PolydivisibleNumber/code/CGCode.php
@@ -0,0 +1,10 @@
+<?php
+$NUMBER = stream_get_line(STDIN, 100 + 1, "\n");
+for ($i = 0; $i < $nbanswer; $i++)
+{
+
+    // Write an answer using echo(). DON'T FORGET THE TRAILING \n
+    // To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+    echo("value\n");
+}
