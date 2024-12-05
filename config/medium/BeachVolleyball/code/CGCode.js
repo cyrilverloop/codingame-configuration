a96000b87da0c2f0aabfc84c1e85f9a23bfacd1f
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-12-05 08:49:24 +0100

    Adding configuration for "🏐 🏖 Beach Volleyball 🏐 🏖".

---
+++ config/medium/BeachVolleyball/code/CGCode.js
@@ -0,0 +1,14 @@
+var inputs = readline().split(' ');
+const STARTX = parseInt(inputs[0]);
+const STARTY = parseInt(inputs[1]);
+const BEACHY = parseInt(readline());
+var inputs = readline().split(' ');
+const BALLX = parseInt(inputs[0]);
+const BALLY = parseInt(inputs[1]);
+const SPEEDLAND = parseInt(readline());
+const SPEEDWATER = parseInt(readline());
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
---
+++ config/medium/BeachVolleyball/code/CGCode.php
@@ -0,0 +1,11 @@
+<?php
+fscanf(STDIN, "%d %d", $STARTX, $STARTY);
+fscanf(STDIN, "%d", $BEACHY);
+fscanf(STDIN, "%d %d", $BALLX, $BALLY);
+fscanf(STDIN, "%d", $SPEEDLAND);
+fscanf(STDIN, "%d", $SPEEDWATER);
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("answer\n");
