3d044cce84a3e13ea5a908bd49c55953d02f32c8
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-02-28 08:32:37 +0100

    Adding configuration for "Magic count of numbers".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -33,6 +33,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "N Pearls Necklace".
 - configuration for "Turing machine".
 - configuration for "Cubax Folding".
+- configuration for "Magic count of numbers".
 
 ### Changed
 - difficulty of "Parse SQL Queries" from easy to medium.
---
+++ config/hard/MagicCountOfNumbers/code/CGCode.js
@@ -0,0 +1,12 @@
+var inputs = readline().split(' ');
+const n = parseInt(inputs[0]);
+const k = parseInt(inputs[1]);
+var inputs = readline().split(' ');
+for (let i = 0; i < k; i++) {
+    const prime = parseInt(inputs[i]);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
