21a18ea83cfb901ce2b4e1a519422c04fa8e9bc4
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-03-05 10:01:37 +0100

    Adding configuration for "Mrs. Knuth - Part II".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Shikaku Skill Builder".
 - configuration for "Timer for Clash of Code".
 - configuration for "Excavating Palindromes".
+- configuration for "Mrs. Knuth - Part II".
 
 ## [1.12.0] - 2025-02-28
 ### Added
---
+++ config/medium/MrsKnuthPartII/code/CGCode.js
@@ -0,0 +1,14 @@
+const teacherAvailability = readline();
+const numStudents = parseInt(readline());
+for (let i = 0; i < numStudents; i++) {
+    const studentData = readline();
+}
+const numTroublesomePairs = parseInt(readline());
+for (let i = 0; i < numTroublesomePairs; i++) {
+    const names = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('schedule');
