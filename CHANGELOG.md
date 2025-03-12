7c04718b52a60350a249c925dae8975ff212850f
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-03-12 10:28:42 +0100

    Adding configuration for "Boggle".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Highest truncated pyramid".
 - configuration for "Sign (programming) language".
 - configuration for "Secret Message Decoding".
+- configuration for "Boggle".
 
 ## [1.12.0] - 2025-02-28
 ### Added
---
+++ config/hard/Boggle/code/CGCode.js
@@ -0,0 +1,13 @@
+const line1 = readline();
+const line2 = readline();
+const line3 = readline();
+const line4 = readline();
+const n = parseInt(readline());
+for (let i = 0; i < n; i++) {
+    const w = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
