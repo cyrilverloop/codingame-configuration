7f0df9c452e61e0a0cb62faff4aac04ba5fd3729
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-03-15 10:19:28 +0100

    Adding configuration for "Neighbor-Sum Grids".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,6 +20,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Boggle".
 - configuration for "TXT2HTML".
 - configuration for "Fractal Carpet".
+- configuration for "Neighbor-Sum Grids".
 
 ## [1.12.0] - 2025-02-28
 ### Added
---
+++ config/hard/NeighborSumGrids/code/CGCode.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 5; i++) {
+    var inputs = readline().split(' ');
+    for (let j = 0; j < 5; j++) {
+        const X = parseInt(inputs[j]);
+    }
+}
+for (let i = 0; i < 5; i++) {
+
+    // Write an answer using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log('solution');
+}
