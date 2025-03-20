0ad1f12cf9546eb43bd1f0eac6d55e2606c947ee
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-03-20 08:51:59 +0100

    Adding configuration for "Elevator".

---
+++ config/hard/Elevator/code/CGCode.js
@@ -0,0 +1,11 @@
+var inputs = readline().split(' ');
+const n = parseInt(inputs[0]);
+const a = parseInt(inputs[1]);
+const b = parseInt(inputs[2]);
+const k = parseInt(inputs[3]);
+const m = parseInt(inputs[4]);
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('IMPOSSIBLE');
