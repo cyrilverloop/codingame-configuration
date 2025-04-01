7cc40e2d72c99b85b1de001eedbef9d885cfca3d
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-04-01 08:38:49 +0200

    Adding configuration for "Shape Outline".

---
+++ config/medium/ShapeOutline/code/CGCode.js
@@ -0,0 +1,11 @@
+const H = parseInt(readline());
+const W = parseInt(readline());
+const S = parseInt(readline());
+for (let i = 0; i < H; i++) {
+    const line = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('Shape vertex X Y');
