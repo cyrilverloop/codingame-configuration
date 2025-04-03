8ddafb46cf78b76ba1a5b98a8cf66f4535038378
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-04-03 08:37:21 +0200

    Adding configuration for "Inside Area".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - configuration for "Shape Outline".
 - configuration for "Ye_ An_th_r W_rd Se_rch".
+- configuration for "Inside Area".
 
 ## [1.13.2] - 2025-03-31
 ### Fixed
---
+++ config/medium/InsideArea/code/CGCode.js
@@ -0,0 +1,11 @@
+const n = parseInt(readline());
+for (let i = 0; i < n; i++) {
+    var inputs = readline().split(' ');
+    const direction = inputs[0];
+    const step = parseInt(inputs[1]);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
