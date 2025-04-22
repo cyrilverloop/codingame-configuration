83fa4b618436383028c1b07301d73fe5211893e2
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-04-22 08:33:39 +0200

    Adding configuration for "Pascal Trapezoid".

---
+++ config/hard/PascalTrapezoid/code/CGCode.js
@@ -0,0 +1,10 @@
+var inputs = readline().split(' ');
+const E = parseInt(inputs[0]);
+const L = parseInt(inputs[1]);
+const N = parseInt(inputs[2]);
+const start = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
