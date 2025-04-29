fceff7885feb0922d24b50d62d782117bbf749f4
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-04-29 09:04:47 +0200

    Adding configuration for "Misère Nim".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -41,6 +41,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Basic Decision Tree - 2".
 - configuration for "Levenshtein distance".
 - configuration for "The greatest number".
+- configuration for "Misère Nim".
 
 ## [1.13.2] - 2025-03-31
 ### Fixed
---
+++ config/hard/MisereNim/code/CGCode.js
@@ -0,0 +1,16 @@
+var inputs = readline().split(' ');
+const N = parseInt(inputs[0]);
+const K = parseInt(inputs[1]);
+for (let i = 0; i < K; i++) {
+    var inputs = readline().split(' ');
+    for (let j = 0; j < N; j++) {
+        const heap = parseInt(inputs[j]);
+    }
+}
+for (let i = 0; i < K; i++) {
+
+    // Write an answer using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log('1:4');
+}
