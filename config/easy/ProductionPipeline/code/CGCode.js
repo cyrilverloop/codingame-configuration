246b52019a6c17f32bfc0e20180cc828ecc32758
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-05-01 09:55:26 +0200

    Adding configuration for "Production Pipeline".

---
+++ config/easy/ProductionPipeline/code/CGCode.js
@@ -0,0 +1,10 @@
+const n = parseInt(readline());
+const k = parseInt(readline());
+for (let i = 0; i < k; i++) {
+    const str = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
