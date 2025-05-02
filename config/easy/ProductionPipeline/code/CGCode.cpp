d0e90f451fbb5dbccaac5c5d7755a380f1dd7af5
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-05-02 09:47:25 +0200

    Adding C++, Java and TypeScript code for "Production Pipeline".

---
+++ config/easy/ProductionPipeline/code/CGCode.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; cin.ignore();
+    int k;
+    cin >> k; cin.ignore();
+    for (int i = 0; i < k; i++) {
+        string str;
+        getline(cin, str);
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}
