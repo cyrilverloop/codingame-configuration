c70fd615f1c8b1fd19af351331d3845d81aeb3dc
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-05-21 10:32:25 +0200

    Adding configuration for "Max Rect".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -26,6 +26,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Hanoi tower".
 - configuration for "Apple tree".
 - configuration for "Breakout".
+- configuration for "Max Rect".
 
 ### Fixed
 - name and link of "The Total is Right - Weird edition".
---
+++ config/hard/MaxRect/code/CGCode.ts
@@ -0,0 +1,11 @@
+var inputs: string[] = readline().split(' ');
+const W: number = parseInt(inputs[0]);
+const H: number = parseInt(inputs[1]);
+for (let i = 0; i < H; i++) {
+    const LINE: string = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('ans');
