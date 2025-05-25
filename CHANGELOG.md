5a0d7e8b505c6154d0b748a23b5bfee352459701
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-05-25 10:16:49 +0200

    Adding configuration for "Doubly solved Rubik’s cube".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,6 +30,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Three little piggies".
 - configuration for "Dumbbells solver".
 - configuration for "The hungry duck - part 2".
+- configuration for "Doubly solved Rubik’s cube".
 
 ### Fixed
 - name and link of "The Total is Right - Weird edition".
---
+++ config/hard/DoublySolvedRubiksCube/code/CGCode.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 11; i++) {
+            String cubeRow = in.nextLine();
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
