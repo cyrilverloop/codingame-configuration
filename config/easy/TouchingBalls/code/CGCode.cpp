05ea5e5ab12b6325736b2588b3426263f010a777
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-06-01 12:04:02 +0200

    Adding configuration for "Touching Balls".

---
+++ config/easy/TouchingBalls/code/CGCode.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; cin.ignore();
+    for (int i = 0; i < n; i++) {
+        int x;
+        int y;
+        int z;
+        int r;
+        cin >> x >> y >> z >> r; cin.ignore();
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "result" << endl;
+}
---
+++ config/easy/TouchingBalls/code/CGCode.ts
@@ -0,0 +1,13 @@
+const N: number = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    var inputs: string[] = readline().split(' ');
+    const x: number = parseInt(inputs[0]);
+    const y: number = parseInt(inputs[1]);
+    const z: number = parseInt(inputs[2]);
+    const r: number = parseInt(inputs[3]);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('result');
