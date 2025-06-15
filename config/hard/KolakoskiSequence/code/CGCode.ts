b4c6b0c80f182c18ad6ae14d042560acc6840361
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-06-15 12:24:20 +0200

    Adding configuration for "Kolakoski Sequence".

---
+++ config/hard/KolakoskiSequence/code/CGCode.ts
@@ -0,0 +1,9 @@
+const N: number = parseInt(readline());
+var inputs: string[] = readline().split(' ');
+const A: number = parseInt(inputs[0]);
+const B: number = parseInt(inputs[1]);
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
