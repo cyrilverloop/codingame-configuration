edbb86b4b7063fbce1271a84ae8930e91d081db4
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-06-27 08:39:00 +0200

    Adding configuration for "The hungry duck - part 1".

---
+++ config/hard/TheHungryDuckPart1/code/CGCode.ts
@@ -0,0 +1,14 @@
+var inputs: string[] = readline().split(' ');
+const W: number = parseInt(inputs[0]);
+const H: number = parseInt(inputs[1]);
+for (let i = 0; i < H; i++) {
+    var inputs: string[] = readline().split(' ');
+    for (let j = 0; j < W; j++) {
+        const food: number = parseInt(inputs[j]);
+    }
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
