781111457b23a192e6ecd9948bcc6a0ad8c465dc
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-07-06 12:10:12 +0200

    Adding configuration for "3D Duck Hunt".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "The Lord of the Annuli".
 - configuration for "Mondrian painting strokes".
 - configuration for "Beautiful base : The most beautiful".
+- configuration for "3D Duck Hunt".
 
 ## [1.16.0] - 2025-06-30
 ### Added
---
+++ config/hard/3DDuckHunt/code/CGCode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 3; i++) {
+        string line;
+        getline(cin, line);
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}
