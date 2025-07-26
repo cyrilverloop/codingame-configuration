51acf047713a3f1d1a7b593e12829507288d788f
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-07-26 11:33:23 +0200

    Adding configuration for "The Crime Scene".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -31,6 +31,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Heart of the City".
 - configuration for "The Lucky Number".
 - configuration for "Hourglass".
+- configuration for "The Crime Scene".
 
 ## [1.16.0] - 2025-06-30
 ### Added
---
+++ config/expert/TheCrimeScene/code/CGCode.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; cin.ignore();
+    for (int i = 0; i < n; i++) {
+        int x;
+        int y;
+        cin >> x >> y; cin.ignore();
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}
