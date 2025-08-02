2246fcbb63b8911aedadac41872684f32c5e1f4f
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-08-02 11:00:22 +0200

    Adding configuration for "Scaleable TicTacToe".

---
+++ config/easy/ScaleableTicTacToe/code/CGCode.js
@@ -0,0 +1,11 @@
+var inputs = readline().split(' ');
+const n = parseInt(inputs[0]);
+const g = parseInt(inputs[1]);
+for (let i = 0; i < n; i++) {
+    const row = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('Answer');
---
+++ config/easy/ScaleableTicTacToe/code/CGCode.php
@@ -0,0 +1,11 @@
+<?php
+fscanf(STDIN, "%d %d", $n, $g);
+for ($i = 0; $i < $n; $i++)
+{
+    $row = stream_get_line(STDIN, $n + 1, "\n");
+}
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("Answer\n");
