b413ce3d9d755861ae9857a5518b862a4ed193fc
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-08-13 09:39:43 +0200

    Adding configuration for "Unfolding paper".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "High-rise buildings".
 - configuration for "Prime Transformations".
 - configuration for "The barnyard".
+- configuration for "Unfolding paper".
 
 ## [1.17.1] - 2025-07-31
 ### Fixed
---
+++ config/expert/UnfoldingPaper/code/CGCode.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; cin.ignore();
+    int w;
+    int h;
+    cin >> w >> h; cin.ignore();
+    for (int i = 0; i < h; i++) {
+        string row;
+        getline(cin, row);
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "1" << endl;
+}
