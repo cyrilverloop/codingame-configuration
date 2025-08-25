425678f26507bad693cd0b820d22b4c05b005877
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-08-25 11:54:24 +0200

    Adding configuration for "Mathematics for big ears".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,6 +30,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - configuration for "Xorandor".
 - configuration for "The two piles difference".
 - configuration for "Sliding puzzle".
+- configuration for "Mathematics for big ears".
 
 ## [1.17.1] - 2025-07-31
 ### Fixed
---
+++ config/expert/MathematicsForBigEars/code/CGCode.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        for (int i = 0; i < n; i++) {
+            String permutation = in.nextLine();
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("order");
+    }
+}
