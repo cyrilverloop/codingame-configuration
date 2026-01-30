2d9059df2a420e3d897570786db3be20beb0c7b6
Cyril VERLOOP <cyril.verloop@webqard.fr>
2026-01-30 13:42:56 +0100

    Adding configuration for "Mountain range".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Added
+- configuration for "Mountain range".
 - configuration for "Distinct Circular Linked Lists".
 - configuration for "The Queen’s Trek (Wythoff’s Game)".
 - configuration for "Simple Diff Tool".
---
+++ config/hard/MountainRange/code/CGCode.ts
@@ -0,0 +1,11 @@
+var inputs: string[] = readline().split(' ');
+const H: number = parseInt(inputs[0]);
+const W: number = parseInt(inputs[1]);
+for (let i = 0; i < H; i++) {
+    const row: string = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('summits');
