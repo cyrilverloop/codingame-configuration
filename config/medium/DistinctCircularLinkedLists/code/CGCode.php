7dc430c7b53d9be334bb1cedb4dc45cacc92d6b4
Cyril VERLOOP <cyril.verloop@webqard.fr>
2026-01-30 12:05:25 +0100

    Adding configuration for "Distinct Circular Linked Lists".

---
+++ config/medium/DistinctCircularLinkedLists/code/CGCode.php
@@ -0,0 +1,11 @@
+<?php
+fscanf(STDIN, "%d", $n);
+for ($i = 0; $i < $n; $i++)
+{
+    fscanf(STDIN, "%d", $node);
+}
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("distinct CLLs?\n");
