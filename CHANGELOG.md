28a7372a902099da4880ef58f09ad9d74f40c322
Cyril VERLOOP <cyril.verloop@webqard.fr>
2026-04-24 12:54:03 +0200

    feat: adds configuration for "The Hole Truth".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - configuration for "Pyramid Stacker".
 - configuration for "Numbers with only two distinct digits".
+- configuration for "The Hole Truth".
 
 ## [1.25.0] - 2026-03-31
 ### Added
---
+++ config/medium/TheHoleTruth/code/CGCode.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int w;
+    int h;
+    cin >> w >> h; cin.ignore();
+    for (int i = 0; i < h; i++) {
+        string row;
+        getline(cin, row);
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}
