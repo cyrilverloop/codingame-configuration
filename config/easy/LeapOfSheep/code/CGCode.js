5293391ea7b1377aecfaffbacf9dad873de46bbe
Cyril VERLOOP <cyril.verloop@webqard.fr>
2026-05-18 19:48:51 +0200

    feat: adds configuration for "Leap of sheep".

---
+++ config/easy/LeapOfSheep/code/CGCode.js
@@ -0,0 +1,10 @@
+const N = parseInt(readline());
+var inputs = readline().split(' ');
+for (let i = 0; i < N; i++) {
+    const height = parseInt(inputs[i]);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('MAXIMAL DIFFICULTY');
---
+++ config/easy/LeapOfSheep/code/CGCode.ts
@@ -0,0 +1,10 @@
+const N: number = parseInt(readline());
+var inputs: string[] = readline().split(' ');
+for (let i = 0; i < N; i++) {
+    const height: number = parseInt(inputs[i]);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('MAXIMAL DIFFICULTY');
